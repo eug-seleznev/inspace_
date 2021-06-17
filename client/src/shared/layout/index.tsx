61551fce19f286f011cbe38a82298465fc5c1bc8
alexdcrv <dikarevaleksandr0@gmail.com>
2021-06-17 15:56:18 +0300

    scss

--- client/src/shared/layout/index.tsx
+++ client/src/shared/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout = () => {
     return (
         <div className={style.layout}  >
             <div className={style.links}  >
-                <Link to='./' className={style.link} onClick={()=>{setOutline('main')}} style={outline==='main'?picked:unpicked}>Главная</Link>
+                <Link to='../../admin' className={style.link} onClick={()=>{setOutline('main')}} style={outline==='main'?picked:unpicked}>Главная</Link>
                 <Link to='../../admin/design' className={style.link} onClick={()=>{setOutline('design')}} style={outline==='design'?picked:unpicked}>Дизайн</Link>
                 <Link to='../../admin/booking' className={style.link} onClick={()=>{setOutline('booking')}} style={outline==='booking'?picked:unpicked}>Бронирование</Link>
                 <Link to='../../admin/service' className={style.link} onClick={()=>{setOutline('service')}} style={outline==='service'?picked:unpicked}>Услуги</Link>
