b1be44269d50193ae43fef81f5183bf887af19eb
eug-seleznev <evgeny.s@buro82.ru>
2021-06-17 12:26:48 +0300

    fix rootStore

--- client/src/stores/RootStore.tsx
+++ client/src/stores/RootStore.tsx
@@ -1,20 +1,16 @@
 import { Container } from "inversify";
 import { TestStore } from "./TestStore";
-import { TUserStore, userStore } from "./user/userStore";
 
 
  export class RootStore {
     public testStore: any;
-    public UserStore: TUserStore;
     public container: Container;
 
     public constructor() {
         this.testStore = new TestStore();
-        this.UserStore = userStore();
         this.container = new Container();
 
         this.container.bind(TestStore).toConstantValue(this.testStore);
-        this.container.bind(userStore).toConstantValue(this.UserStore);
     }
 
 }    
